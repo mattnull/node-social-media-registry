9f6c3ed8c46bc99f85d815445ced2c73b87ebd41
Matt <hello@mattnull.com>
2013-02-20 19:12:25 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+var request = require('request');
+
+// Social Media Registry API
+var SMR = function(){
+	this.baseURI = '';
+};
+
+SMR.prototype.request = function(url, callback){
+	request.get(url, function(err, res, body){
+		callback(err, body);
+	});
+};
+
+SMR.prototype.getRecentDatasets = function(count, callback){
+	count = count || false;
+	var endpoint = 'get_recent_datasets';
+	endpoint = count ? endpoint+'?count='+count : endpoint;
+	var url = this.baseURI+endpoint;
+	
+	this.request(url, callback);
+};
+
+module.exports = SMR;
+
